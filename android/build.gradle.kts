365af06141790e4ade7bfe66674f8e87868b3e6d
Arvin_Fox <arvin250cc@gmail.com>
2026-03-11 10:49:02 +0530

    small fix on build.gradle.kts on android level

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,16 +1,13 @@
+plugins {
+    // Add the dependency for the Google services Gradle plugin
+    id("com.google.gms.google-services") version "4.4.3" apply false
+}
+
 allprojects {
     repositories {
         google()
         mavenCentral()
     }
-}
-
-plugins {
-  // ...
-
-  // Add the dependency for the Google services Gradle plugin
-  id("com.google.gms.google-services") version "4.4.3" apply false
-
 }
 
 val newBuildDir: Directory =

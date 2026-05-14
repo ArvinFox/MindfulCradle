2de4e59d23e1663c0a523e794d61606f5da1f858
Arvin_Fox <arvin250cc@gmail.com>
2026-05-14 12:41:28 +0530

    fix: update NDK version to 28.2.13676358 in build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -37,7 +37,7 @@ dependencies {
 android {
     namespace = "com.example.mamamind"
     compileSdk = 36
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
